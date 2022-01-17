b2710f3c86257f5b74f00c1d672d3110ca0fd045
Victor Amaral <vic-am@hotmail.com>
2022-01-17 00:34:20 -0300

    Implementa o AddProductFragment.kt

--- app/src/main/java/com/victoramaral/whitelabel/ui/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/victoramaral/whitelabel/ui/addproduct/AddProductFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputLayout
 import com.victoramaral.whitelabel.databinding.AddProductFragmentBinding
 import com.victoramaral.whitelabel.util.CurrencyTextWatcher
 
@@ -14,14 +15,17 @@ class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: AddProductFragmentBinding? = null
     private val binding get() = _binding!!
+
     private var imageURI: Uri? = null
+
+    private lateinit var viewModel: AddProductViewModel
+
     private val getContent =
         registerForActivityResult(ActivityResultContracts.GetContent()) {
             imageURI = it
             binding.imageProduct.setImageURI(it)
-        }
 
-    private lateinit var viewModel: AddProductViewModel
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,27 +33,56 @@ class AddProductFragment : BottomSheetDialogFragment() {
     ): View? {
         _binding = AddProductFragmentBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setListener()
+        observeViewModelEvents()
+    }
+
+    private fun observeViewModelEvents() {
+        viewModel.imageUriErrorResId.observe(viewLifecycleOwner) {
+            binding.imageProduct.setBackgroundResource(it)
+        }
+
+        viewModel.descriptionFieldErrorResId.observe(viewLifecycleOwner) {
+            binding.inputLayoutDescription.setError(it)
+        }
+
+        viewModel.priceFieldErrorResId.observe(viewLifecycleOwner) {
+            binding.inputLayoutPrice.setError(it)
+        }
+
     }
 
     private fun setListener() {
         binding.imageProduct.setOnClickListener {
             chooseImage()
         }
+
         binding.buttonAddProduct.setOnClickListener {
             val description = binding.inputDescription.text.toString()
             val price = binding.inputPrice.text.toString()
+            viewModel.createProduct(description, price, imageURI)
         }
+
         binding.inputPrice.run {
             addTextChangedListener(CurrencyTextWatcher(this))
         }
+
     }
 
     private fun chooseImage() {
         getContent.launch("image/*")
+
     }
+
+    private fun TextInputLayout.setError(stringResId: Int?) {
+        error = if (stringResId != null) {
+            getString(stringResId)
+        } else null
+    }
+
 }
--- app/src/main/java/com/victoramaral/whitelabel/ui/addproduct/AddProductViewModel.kt
+++ app/src/main/java/com/victoramaral/whitelabel/ui/addproduct/AddProductViewModel.kt
@@ -1,5 +1,61 @@
 package com.victoramaral.whitelabel.ui.addproduct
 
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.victoramaral.whitelabel.R
+import com.victoramaral.whitelabel.domain.usecase.CreateProductUseCase
+import com.victoramaral.whitelabel.util.fromCurrency
+import kotlinx.coroutines.launch
 
-class AddProductViewModel : ViewModel() {}
+class AddProductViewModel(
+    private val createProductUseCase: CreateProductUseCase
+) : ViewModel() {
+
+    private var isFormValid = true
+
+    private val _imageUriErrorResId = MutableLiveData<Int>()
+    val imageUriErrorResId: LiveData<Int> = _imageUriErrorResId
+
+    private val _descriptionFieldErrorResId = MutableLiveData<Int?>()
+    val descriptionFieldErrorResId: LiveData<Int?> = _descriptionFieldErrorResId
+
+    private val _priceFieldErrorResId = MutableLiveData<Int?>()
+    val priceFieldErrorResId: LiveData<Int?> = _priceFieldErrorResId
+
+    fun createProduct(description: String, price: String, imageURI: Uri?) =
+        viewModelScope.launch {
+            _imageUriErrorResId.value = getDrawableBackgroundIfFieldIsEmpty(imageURI)
+            _descriptionFieldErrorResId.value = getErrorStringIfFieldIsEmpty(description)
+            _priceFieldErrorResId.value = getErrorStringIfFieldIsEmpty(price)
+
+            if (isFormValid) {
+                try {
+                    val product =
+                        createProductUseCase(description, price.fromCurrency(), imageURI!!)
+                } catch (e: Exception) {
+                    Log.d("CreateProduct", e.toString())
+                }
+            }
+
+        }
+
+    private fun getErrorStringIfFieldIsEmpty(value: String): Int? {
+        return if (value.isEmpty()) {
+            isFormValid = false
+            R.string.add_product_field_error
+        } else null
+
+    }
+
+    private fun getDrawableBackgroundIfFieldIsEmpty(value: Uri?): Int {
+        return if (value == null) {
+            isFormValid = false
+            R.drawable.background_product_image_error
+        } else R.drawable.background_product_image
+
+    }
+}
